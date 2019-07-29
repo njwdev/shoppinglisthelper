bee1fd7251ed8f93facca9fed9793ec2d9dd2acb
njwdev <nwwebd@gmail.com>
2019-07-29 22:49:55 +0200

    changed navbarstyles casing

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import navbarStyles from './styles/navbarStyles';
+import NavbarStyles from './styles/navbarStyles';
 
 const data = (text, link) => ({ text, link });
 const nonAuthButtons = [
@@ -59,4 +59,4 @@ const Navbar = props => {
   );
 };
 
-export default withStyles(navbarStyles)(Navbar);
+export default withStyles(NavbarStyles)(Navbar);
