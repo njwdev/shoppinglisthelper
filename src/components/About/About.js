c560547ea3fbf9525e292c826ff1a6f522ccf485
njwdev <nwwebd@gmail.com>
2019-08-10 17:57:41 +0200

    added about data and refactored about file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,31 +4,23 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import { AboutData } from './AboutData';
 
 const About = () => (
   <PageContainer>
-    <ExpansionPanel>
-      <ExpansionPanelSummary>
-        <Typography> What is Shopping List Helper?</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Typography>
-          {' '}
-          Shopping List Helper is an App to help people coordinate their
-          shopping lists
-        </Typography>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
+    {AboutData.map(question => {
+      return (
+        <ExpansionPanel key={question.question}>
+          <ExpansionPanelSummary>
+            <Typography>{question.question}</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>{question.answer}</Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      );
+    })}
     {/* <Divider /> */}
-    <ExpansionPanel>
-      <ExpansionPanelSummary>
-        <Typography> How do I use Shopping List Helper?</Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        <Typography>......</Typography>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
   </PageContainer>
 );
 export default About;
